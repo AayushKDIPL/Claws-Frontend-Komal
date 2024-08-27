c622132c7c53b4baef5d5f366ed35039187facae
AayushKDIPL <aayush@kanakdrishtiinfo.com>
2024-08-27 17:35:01 +0530

    Delete src/Script.js

--- src/Script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.navbar-toggler').on('click', function() {
-      $(this).toggleClass('collapsed');
-      $('.navbar-collapse').toggleClass('show');
-    });
-  });
